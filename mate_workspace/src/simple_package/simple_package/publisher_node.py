f4863b53eba9c99d28bf51948ee265ae8a742f97
Jesus Betancourt <jbeta02@gmail.com>
2024-10-22 18:13:47 -0700

    create publisher and subscriber nodes

---
+++ mate_workspace/src/simple_package/simple_package/publisher_node.py
@@ -0,0 +1,43 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class PublisherNode(Node):
+
+    def __init__(self):
+        super().__init__('publisher_node') # run Node constructor
+        self.publisher_ = self.create_publisher(String, 'first_topic', 10) # publish to topic named first_topic
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        # send message over topic
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)
+        # display message sent
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    # create publisher node obj
+    publisher_node = PublisherNode()
+
+    # run Node
+    rclpy.spin(publisher_node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    publisher_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
