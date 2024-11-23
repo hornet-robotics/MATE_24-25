6d89b4cb6d27a0f1770b2d1b6f4b70886fb06438
Jesus Betancourt <jbeta02@gmail.com>
2024-11-23 14:04:03 -0800

    adjust motor pwm values. add m2 which was missing. properly place main function

--- mate_workspace/src/vector_drive/vector_drive/vector_drive.py
+++ mate_workspace/src/vector_drive/vector_drive/vector_drive.py
@@ -15,8 +15,6 @@ class VectorDrive(Node):
             10)
         self.subscription  # prevent unused variable warning
 
-        self.radius = 0 # TODO add value here
-
 
     def listener_callback(self, msg):
 
@@ -30,13 +28,13 @@ class VectorDrive(Node):
         right_trigger = msg.data[5]
 
         # scale to match pwm
-        pwm_scale = 1
+        pwm_scale = 255
         # 2d
         forward_back = pwm_scale * joystick_left_y
         strafe = pwm_scale * -joystick_left_x
         yaw = pwm_scale * -joystick_right_x
         # 3d
-        up_down = pwm_scale * (right_trigger - left_trigger) # TODO adjust trigger values to make sense
+        up_down = pwm_scale * (right_trigger - left_trigger)
         pitch = pwm_scale * joystick_right_y
 
         # give values to motors
@@ -46,35 +44,35 @@ class VectorDrive(Node):
         #     m0 //--m5--\\ m3
         #  +y       ----
         #     m1 \\--m4--// m2
+        # counter-clockwise is positive for yaw rotation
+        # nose up is positive in pitch rotation
 
-        m0 = forward_back - strafe -(2 * self.radius * yaw) # verify turning logic here
-        m1 = forward_back + strafe -(2 * self.radius * yaw)
-        m2 = forward_back + strafe +(2 * self.radius * yaw)
-        m3 = forward_back - strafe +(2 * self.radius * yaw)
-        m4 = up_down - (2 * self.radius * pitch)
-        m5 = up_down + (2 * self.radius * pitch)
+        m0 = forward_back - strafe - yaw
+        m1 = forward_back + strafe - yaw
+        m2 = forward_back - strafe + yaw
+        m3 = forward_back + strafe + yaw
+        m4 = up_down - pitch
+        m5 = up_down + pitch
 
-        # TODO add motor values to motors.setvalue
+        # TODO set motor pwm values here
 
-
-        # display message received
-        self.get_logger().info('Heard: "%s"' % msg.data)
+        self.get_logger().info('From joystick_topic I heard: "%s"' % msg.data)
 
 
-    def main(args=None):
-        rclpy.init(args=args)
+def main(args=None):
+    rclpy.init(args=args)
 
-        # create subscriber node obj
-        vector_drive_node = VectorDrive()
+    # create subscriber node obj
+    vector_drive_node = VectorDrive()
 
-        # run Node
-        rclpy.spin(vector_drive_node)
+    # run Node
+    rclpy.spin(vector_drive_node)
 
-        # Destroy the node explicitly
-        # (optional - otherwise it will be done automatically
-        # when the garbage collector destroys the node object)
-        vector_drive_node.destroy_node()
-        rclpy.shutdown()
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    vector_drive_node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
